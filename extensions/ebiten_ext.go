597f2b65d0a102f1f5c70f35ca1d9f11e575f9c5
Dlcuy22 <alifqwik@gmail.com>
2026-01-03 12:21:36 +0700

    feat: Complete ebiten game engine template
    Added a complete ebiten template with a simple demo window that displays a welcome message and a snake graphic. The boilerplate is minimal and easy to delete, allowing developers to quickly start their game project.
    
    Features:
    - Simple game window (640x480) with dark background
    - Welcome message: 'It works! Remove the boilerplate code to start your project'
    - Simple green snake graphic as a visual demo
    - Clean Game struct with Update/Draw/Layout methods
    - Uses ebiten/v2 (latest version)
    - Easy to understand and remove - just delete the demo code and start coding
    
    The template serves as a quick starting point for ebiten-based games while being minimal enough that developers can easily strip it down and build their own game logic.

--- extensions/ebiten_ext.go
+++ extensions/ebiten_ext.go
@@ -1,26 +1,71 @@
 package extensions
 
-// func init() {
-// 	RegisterTemplate(ebitenTemplate{})
-// }
+func init() {
+	RegisterTemplate(ebitenTemplate{})
+}
 
-// type ebitenTemplate struct{}
+type ebitenTemplate struct{}
 
-// func (ebitenTemplate) Name() string        { return "Ebiten Engine" }
-// func (ebitenTemplate) Description() string { return "Simple Ebiten Engine Template" }
-// func (ebitenTemplate) RootDir() string     { return "" }
-// func (ebitenTemplate) Dependencies() []string {
-// 	return []string{"github.com/hajimehoshi/ebiten", "fmt"}
-// }
-// func (ebitenTemplate) Files(projectName string) map[string]string {
-// 	return map[string]string{
-// 		"main.go": `package main
-// 		func main() {
-// 		fmt.Println("karbit")
-// 		}
-// 		`,
-// 	}
-// }
+func (ebitenTemplate) Name() string        { return "ebiten" }
+func (ebitenTemplate) Description() string { return "Ebiten game engine template" }
+func (ebitenTemplate) RootDir() string     { return "" }
+func (ebitenTemplate) Dependencies() []string {
+	return []string{"github.com/hajimehoshi/ebiten/v2"}
+}
+func (ebitenTemplate) Files(projectName string) map[string]string {
+	return map[string]string{
+		"main.go": `package main
 
+import (
+	"image/color"
+	"log"
 
-//TODO LATER
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+// Game represents the game state
+type Game struct{}
+
+// Update updates the game state
+func (g *Game) Update() error {
+	return nil
+}
+
+// Draw renders the game
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Background
+	screen.Fill(color.RGBA{15, 23, 42, 255})
+	
+	// Message
+	ebitenutil.DebugPrintAt(screen, "It works!", 20, 20)
+	ebitenutil.DebugPrintAt(screen, "Remove the boilerplate code to start your project", 20, 40)
+	
+	// Simple snake drawing
+	snakeColor := color.RGBA{34, 197, 94, 255}
+	// Snake body
+	ebitenutil.DrawRect(screen, 20, 80, 20, 20, snakeColor)
+	ebitenutil.DrawRect(screen, 40, 80, 20, 20, snakeColor)
+	ebitenutil.DrawRect(screen, 60, 80, 20, 20, snakeColor)
+	ebitenutil.DrawRect(screen, 80, 80, 20, 20, snakeColor)
+	// Snake eyes
+	ebitenutil.DrawRect(screen, 85, 85, 3, 3, color.White)
+	ebitenutil.DrawRect(screen, 92, 85, 3, 3, color.White)
+}
+
+// Layout returns the game's screen size
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 640, 480
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("` + projectName + `")
+	
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
+`,
+	}
+}
